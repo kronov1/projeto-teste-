745e3e5a94d8f365154a33fab0fb02f7acfe261b
bruno <brunooliveira22br74@gmail.com>
2023-05-26 19:38:03 -0300

    criação do swiper

---
+++ js/swiper.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 18,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
